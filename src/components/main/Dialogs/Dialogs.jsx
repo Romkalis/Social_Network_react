0fd55cc1417b06371ac291f4caff4e7f928c4057
RLis <lisss17@mail.ru>
2023-09-23 18:44:07 +0500

    add MAP to mock data

--- src/components/main/Dialogs/Dialogs.jsx
+++ src/components/main/Dialogs/Dialogs.jsx
@@ -1,25 +1,23 @@
 import style from "./Dialogs.module.css";
 import Dialog from "./Dialog/Dialog";
 import Message from "./Message/Message";
-import { mockData } from "../../mock/mock";
+import { dialogsData, messagesData } from "../../mock/mock";
 
 const Dialogs = (props) => {
+
+  let dialogsElements = dialogsData.map( (dialog) => <Dialog name={dialog.name} id={dialog.id}></Dialog>)
+  let messagesElements = messagesData.map( (message) => <Message text={message.text} id={message.id}></Message>)
   return (
     <section className={style.dialogs}>
       <ul className={style.dialogsList}>
          
-        <Dialog name="Dima" id="1"></Dialog>
-        <Dialog name="Roma" id="2"></Dialog>
-        <Dialog name="Valery" id="3"></Dialog>
-        <Dialog name="Sasha" id="4"></Dialog>
-        <Dialog name="Tom" id="5"></Dialog>
-        <Dialog name="Jack" id="6"></Dialog>
+         {dialogsElements}
 
       </ul>
       <ul className={style.messages}>
-        <Message text="Hi"></Message>
-        <Message text="How are you"></Message>
-        <Message text="i like chicken"></Message>
+
+        {messagesElements}
+        
       </ul>
     </section>
   );
--- src/components/mock/mock.js
+++ src/components/mock/mock.js
@@ -1,4 +1,4 @@
-export let mockData =[
+export let dialogsData =[
     {id: 1, name: "Dima"},
     {id: 2, name: "Roma"},
     {id: 3, name: "Andrew"},
@@ -6,4 +6,20 @@ export let mockData =[
     {id: 5, name: "Anton"},
     {id: 6, name: "Lymen"},
     {id: 7, name: "Yukki"},
+]
+
+export let messagesData = [
+    {id: 1, text: "Hey! I'm First"},
+    {id: 2, text:"Ololo trololo im driver NLO"},
+    {id: 3, text:"wof wof?"},
+    {id: 4, text:"Meow"},
+    {id: 5, text:"Hi guys, how are your tails?"},
+]
+
+export let postsData = [
+    {id: 1, post: "Hey! I'm First", like: 5},
+    {id: 2, post:"Ololo trololo im driver NLO", like: 25},
+    {id: 3, post:"wof wof?", like: 15},
+    {id: 4, post:"Meow", like: 2},
+    {id: 5, post:"Hi guys, how are your tails?", like: 1},
 ]
